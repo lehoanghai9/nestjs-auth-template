fd156e0b0abdea855581a9ad29e94d6fb8459161
lehoanghai9 <hyperminister@gmail.com>
2025-01-20 23:24:05 +0100

    (feat) actually, not its done, the guard canActivate method should be async

--- src/common/guards/subscription.guard.ts
+++ src/common/guards/subscription.guard.ts
@@ -7,7 +7,6 @@ import {
    UnauthorizedException,
 } from '@nestjs/common';
 import { type UserDetailRequest } from '../types/userdetail-request.type';
-import { Observable } from 'rxjs';
 import { SubscriptionService } from 'src/subscription/subscription.service';
 
 @Injectable()
@@ -18,25 +17,25 @@ export class SubscriptionGuard implements CanActivate {
       private readonly subscriptionService: SubscriptionService,
    ) {}
 
-   canActivate(
+   async canActivate(
       context: ExecutionContext,
-   ): boolean | Promise<boolean> | Observable<boolean> {
+   ):  Promise<boolean> {
       const request: UserDetailRequest = context.switchToHttp().getRequest();
 
       const userId = request.userId;
 
       if (!userId) {
-        this.logger.error('Invalid Token.');
+         this.logger.error('Invalid Token.');
          throw new UnauthorizedException('Invalid Token.');
       }
 
-      const isSubscribed = this.subscriptionService.isUserSubscribedToProduct(
+      const isSubscribed = await this.subscriptionService.isUserSubscribedToProduct(
          userId,
          'prod_QuS8xpAeBv58EN',
       );
 
       if (!isSubscribed) {
-        this.logger.error('User is not subscribed to the product.');
+         this.logger.error('User is not subscribed to the product.');
          throw new UnauthorizedException(
             'User is not subscribed to the product.',
          );
